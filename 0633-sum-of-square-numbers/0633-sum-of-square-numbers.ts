82feeb0c11e8363deedbca19406ddb64320ecee6
Mayank Mishra <mayank0801mishra@gmail.com>
2024-08-02 01:02:29 +0530

    Time: 70 ms (52.58%), Space: 51.6 MB (69.48%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.ts
@@ -0,0 +1,13 @@
+function judgeSquareSum(c: number): boolean {
+    for(let a = 0; a*a <= c; a++){
+        
+        const b = Math.sqrt(c - a*a);
+    
+      if(b == Math.floor(b)){
+          return true;
+      }
+        
+    }
+    
+    return false;
+};
