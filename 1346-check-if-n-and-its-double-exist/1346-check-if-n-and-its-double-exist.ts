0e28860aa6baa2da7252dcda5f62043e2499845d
Mayank Mishra <mayank0801mishra@gmail.com>
2024-12-02 00:50:27 +0530

    Time: 2 ms (64.04%), Space: 52.1 MB (27.93%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.ts
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.ts
@@ -1,16 +1,23 @@
+// Function to check if the array contains any element such that 
+// an element is double another element in the array.
 function checkIfExist(arr: number[]): boolean {
-    
+     // Set to keep track of the elements we've seen so far.
     const  seenElements = new Set<number>();
     
     for(const value of arr){
+ // Check if we've seen an element that is double the current value
+ // or if we've seen an element that is half the current value.
         if(seenElements.has(value*2) || (value%2 === 0 && 
                                         
                                         seenElements.has(value/2))){
+ // If such an element exists, return true.
             return true;
         }
-        
+ // Add the current value to the set of seen elements.
         seenElements.add(value);
     }
-    
+
+        // If no such pair is found, return false.
+
     return false;
 };
