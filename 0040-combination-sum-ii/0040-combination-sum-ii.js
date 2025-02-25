d86d4f98379ece5126cdb60820bb8bf966802b56
Mayank Mishra <mayank0801mishra@gmail.com>
2025-02-26 05:27:33 +0530

    Time: 6 ms (43.64%), Space: 58.4 MB (5.09%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum2 = function(candidates, target) {
+    let res = [];
+    candidates.sort((a,b)=>a-b);
+    function backtrack(index,currSum,curr){
+        if(currSum === target){
+         res.push([...curr]);
+        return;
+        }
+           if(currSum > target || index > candidates.length-1){
+        return;
+    }
+    let hash = {};
+    for(let j = index;j<candidates.length;j++){
+        if(!hash[candidates[j]]){
+            hash[candidates[j]] = true;
+            curr.push(candidates[j]);
+            backtrack(j+1,currSum+candidates[j],curr);
+            curr.pop();
+        }
+    }
+
+   
+       
+    }
+     backtrack(0,0,[]);
+    return res;
+ 
+};
