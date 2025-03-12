5d867ba8e9c712159ff9d7fd206f471733fe5cd8
Mayank Mishra <mayank0801mishra@gmail.com>
2025-03-12 15:18:47 +0530

    Time: 1038 ms (11.56%), Space: 70.8 MB (30.06%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string[]} dictionary
+ * @param {string} sentence
+ * @return {string}
+ */
+var replaceWords = function(dictionary, sentence) {
+          const rootSet = new Set(dictionary);
+          const words = sentence.split(" ");
+
+          for(let i = 0;i<words.length;i++){
+            for(let j = 1;j<=words[i].length;j++){
+                const prefix = words[i].substring(0,j);
+                if(rootSet.has(prefix)){
+                    words[i] = prefix;
+                    break;
+                }
+            }
+          } 
+
+          return words.join(" ");
+};
