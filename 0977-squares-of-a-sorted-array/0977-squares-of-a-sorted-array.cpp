e0c7e22cd1dfe2008b04af80388bc9d029b0241c
Mayank Mishra <mayank0801mishra@gmail.com>
2025-03-12 13:46:37 +0530

    Time: 0 ms (100%), Space: 30.2 MB (85.79%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -7,13 +7,38 @@ public:
 
          //m-1
 
-           for(int i = 0;i<nums.size();i++){
-            nums[i] = (nums[i] * nums[i]);
-           }
+        //    for(int i = 0;i<nums.size();i++){
+        //     nums[i] = (nums[i] * nums[i]);
+        //    }
 
-           sort(begin(nums),end(nums));
+        //    sort(begin(nums),end(nums));
 
-           return nums;
+        //    return nums;
 
+       int n = nums.size();
+
+       vector<int> result(n);
+
+       int i = 0, j = n-1;
+
+
+
+       int k = n-1; 
+
+         while(k >=0){
+            int a =  nums[i] * nums[i];
+            int b = nums[j] *   nums[j];
+
+            if(a > b){
+                result[k] = a;
+                i++;
+            }else{
+                result[k] = b;
+                j--;
+            }
+
+            k--;
+         }
+         return result;
     }
 };
