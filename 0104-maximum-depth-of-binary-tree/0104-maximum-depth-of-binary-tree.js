c99d40d145b0691651238c3ab743b553915c7861
Mayank Mishra <mayank0801mishra@gmail.com>
2025-03-21 03:23:58 +0530

    Time: 0 ms (100%), Space: 56.1 MB (47%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -11,10 +11,11 @@
  * @return {number}
  */
 var maxDepth = function(root) {
+
      if(!root) return 0;
-        let left = maxDepth(root.left)
-        
-        let right = maxDepth(root.right)
-        return Math.max(left,right) + 1;
+         let left = maxDepth(root.left)
+         let right = maxDepth(root.right)
+
+         return Math.max(left,right) +1;
     
 };
