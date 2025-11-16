6618e3802ea859dfc7bce0be398981c8c4f9a9b7
Mayank Mishra <mayank0801mishra@gmail.com>
2025-11-16 23:36:07 +0530

    Time: 0 ms (100%), Space: 11.6 MB (39.33%) - LeetHub

---
+++ 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numSub(string s) {
+         
+         const int M = 1e9+7;
+         long long result = 0;
+         long long count1 = 0;
+
+         for(char ch : s){
+            if(ch == '1'){
+                count1++;
+            }
+            else{
+                result = (result + count1*(count1 + 1)/2) % M;
+                count1 = 0;
+
+            }
+         }
+
+         result = (result + count1 * (count1 + 1)/2) % M;
+
+         return (int)result;
+
+    }
+};
