262e7c247b5036768cb427b2ff793e61198c7d5b
Mayank Mishra <mayank0801mishra@gmail.com>
2025-11-19 21:00:55 +0530

    Time: 3 ms (43.47%), Space: 14 MB (89.2%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
     int findFinalValue(vector<int>& nums, int original) {
-        unordered_map<int,int> mp;
+        // unordered_map<int,int> mp;
 
-        for(int i : nums){
-            mp[i]++;
-        }
+        // for(int i : nums){
+        //     mp[i]++;
+        // }
 
-        while(mp[original] > 0){
-            original*=2;
+        // while(mp[original] > 0){
+        //     original*=2;
+        // }
+        // return original;
+        sort(nums.begin(),nums.end());
+
+        for(int i:nums){
+             if(i == original){
+                original*= 2;
+             }
         }
-        return original;
+            return original;
     }
+
+
+
 };
