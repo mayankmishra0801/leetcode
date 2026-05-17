57639b4542bf3849a146cf8b5e1996bdc74e8019
Mayank Mishra <mayank0801mishra@gmail.com>
2026-05-17 17:40:34 +0530

    Time: 0 ms (100%), Space: 53.3 MB (76.97%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    let l = 0;
+    let r = nums.length -1;
+
+    while(l < r){
+        let mid =  Math.floor((r+l)/2);
+
+        if(nums[mid] > nums[r]){
+            l = mid + 1;
+
+        }else{
+             r = mid;
+        }
+    }
+
+    return nums[l];
+    
+};
