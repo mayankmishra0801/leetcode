8dc45d16c7e16345f0fc31240617fedfa8a940e8
Mayank Mishra <mayank0801mishra@gmail.com>
2026-07-12 15:23:42 +0530

    Time: 2 ms (83.87%), Space: 55.1 MB (72.48%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -3,19 +3,41 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-     let stk = [];
-     for(const c of s){
-        if(c == '(' || c == '{' || c == '['){
-            stk.push(c);
-    }else if(stk.length == 0 || !match(stk[stk.length-1],c)){
-         return false;
-    }else{
-        stk.pop();
+    //  let stk = [];
+    //  for(const c of s){
+    //     if(c == '(' || c == '{' || c == '['){
+    //         stk.push(c);
+    // }else if(stk.length == 0 || !match(stk[stk.length-1],c)){
+    //      return false;
+    // }else{
+    //     stk.pop();
+    // }
+    //  }
+    //  return stk.length == 0;
+
+
+
+    const stack = [];
+
+    const map = {
+        ')' :'(',
+        '}' : '{',
+        ']' : '['
+    };
+
+    for(let ch of s){
+        if(ch === '(' || ch === '{' || ch === '['){
+            stack.push(ch);
+        }else{
+            if(stack.pop() !== map[ch]){
+                return false;
+            }
+        }
     }
-     }
-     return stk.length == 0;
+
+    return stack.length === 0;
 };
 
-function match(l,r){
-    return (l == '(' && r == ')') || (l == '[' && r == ']') || (l == '{' && r == '}');
-}
+// function match(l,r){
+//     return (l == '(' && r == ')') || (l == '[' && r == ']') || (l == '{' && r == '}');
+// }
