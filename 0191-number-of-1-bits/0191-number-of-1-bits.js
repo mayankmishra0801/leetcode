789cc31ead5b8acecf3aaa81d8539e26183c76fa
Mayank Mishra <mayank0801mishra@gmail.com>
2026-07-13 00:16:33 +0530

    Time: 0 ms (100%), Space: 55.1 MB (33.64%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.js
+++ 0191-number-of-1-bits/0191-number-of-1-bits.js
@@ -3,11 +3,20 @@
  * @return {number}
  */
 var hammingWeight = function(n) {
-    let count = 0;
+    // let count = 0;
 
-    while(n !== 0){
-        n = n & (n-1);
-        count++;
+    // while(n !== 0){
+    //     n = n & (n-1);
+    //     count++;
+    // }
+
+    // return count;
+
+
+    let count = 0;
+    while(n > 0){
+        count += n & 1;
+        n = n >> 1;
     }
 
     return count;
