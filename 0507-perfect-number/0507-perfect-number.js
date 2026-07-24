89b632b720aab9474007e7ceaedc5532e6954e81
Mayank Mishra <mayank0801mishra@gmail.com>
2026-07-25 02:15:22 +0530

    Time: 1 ms (89.64%), Space: 55.4 MB (30.74%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var checkPerfectNumber = function(num) {
+       if (num === 1) return false;
+
+    let sum = 1; // 1 is always a divisor
+
+    for (let i = 2; i * i <= num; i++) {
+      
+
+        if(num % i === 0){
+            sum += i;
+            if(i !== num / i){
+                sum += num / i;
+            }
+        }
+    }
+
+    return sum === num;
+};
