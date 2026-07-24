51fa8b9a815192af4c3ca2c888bf496af5d6f69b
Mayank Mishra <mayank0801mishra@gmail.com>
2026-07-25 04:04:10 +0530

    Time: 0 ms (100%), Space: 56.5 MB (14.29%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.js
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.js
@@ -4,20 +4,30 @@
  * @return {character}
  */
 var nextGreatestLetter = function(letters, target) {
-    let left = 0;
-    let right = letters.length - 1;
+    // let left = 0;
+    // let right = letters.length - 1;
 
-    while(left <= right){
-        let mid = Math.floor((left+right)/2);
+    // while(left <= right){
+    //     let mid = Math.floor((left+right)/2);
 
-        if(letters[mid] <= target){
-            left = mid + 1;
+    //     if(letters[mid] <= target){
+    //         left = mid + 1;
         
-        }else{
-            right = mid - 1;
+    //     }else{
+    //         right = mid - 1;
+    //     }
+    // }
+
+
+    // return left < letters.length ? letters[left] : letters[0];
+
+
+
+    for(let ch of letters){
+        if(ch > target){
+            return ch;
         }
     }
 
-
-    return left < letters.length ? letters[left] : letters[0];
+    return letters[0];
 };
