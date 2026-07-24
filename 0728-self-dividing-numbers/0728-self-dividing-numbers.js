52fc8243b424a56b70b2bc98fb06d88dd12e366f
Mayank Mishra <mayank0801mishra@gmail.com>
2026-07-25 03:58:21 +0530

    Time: 3 ms (62.41%), Space: 55.2 MB (54.89%) - LeetHub

---
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number[]}
+ */
+var selfDividingNumbers = function(left, right) {
+    const result = [];
+
+    for(let num = left; num <= right;num++){
+        let temp = num;
+
+        let isSelfDividing = true;
+
+        while(temp > 0){
+            const digit = temp % 10;
+
+            if(digit === 0 || num % digit !== 0){
+                isSelfDividing = false;
+                break;
+            }
+
+            temp = Math.floor(temp/10);
+        }
+
+        if(isSelfDividing){
+            result.push(num);
+        }
+    }
+
+    return result;
+};
