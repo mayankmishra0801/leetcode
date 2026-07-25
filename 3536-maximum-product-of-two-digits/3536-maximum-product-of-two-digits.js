9a435bc9be68a708f8d1a66ad3162c7577bd0ab7
Mayank Mishra <mayank0801mishra@gmail.com>
2026-07-25 09:51:46 +0530

    Time: 0 ms (100%), Space: 56.9 MB (38.3%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var maxProduct = function(n) {
+    let first = 0;
+    let second = 0;
+
+    while(n > 0){
+        let digit = n % 10;
+
+        if(digit >= first){
+            second = first;
+            first = digit;
+        }else if(digit >second){
+            second = digit;
+        }
+
+        n = Math.floor(n / 10);
+    }
+
+    return first * second;
+    
+};
