e8c2b639342d9f6218194e5380af63486759a6b9
Mayank Mishra <mayank0801mishra@gmail.com>
2026-08-01 22:53:47 +0530

    Time: 0 ms (100%), Space: 53.6 MB (76.74%) - LeetHub

---
+++ 0824-goat-latin/0824-goat-latin.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} sentence
+ * @return {string}
+ */
+var toGoatLatin = function(sentence) {
+    const vowels = new Set(['a','e','i','o','u','A','E','I','O','U']);
+
+
+    const words = sentence.split(" ");
+
+    const result = [];
+
+    for(let i = 0; i < words.length; i++){
+        let word = words[i];
+
+        if(vowels.has(word[0])){
+            word = word + "ma";
+        }else{
+            word = word.slice(1) + word[0]+ "ma"
+        }
+
+        word += "a".repeat(i + 1);
+
+
+        result.push(word);
+    }
+
+
+    return result.join(" ");
+};
