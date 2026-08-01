3a7d656c10e559fb0152ae173056f0c1647d838b
Mayank Mishra <mayank0801mishra@gmail.com>
2026-08-01 19:26:54 +0530

    Time: 120 ms (21.85%), Space: 159 MB (5.92%) - LeetHub

---
+++ 0706-design-hashmap/0706-design-hashmap.js
@@ -0,0 +1,37 @@
+
+var MyHashMap = function() {
+    this.data = Array(1000001).fill(-1);
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+MyHashMap.prototype.put = function(key, value) {
+    this.data[key] = value;
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+MyHashMap.prototype.get = function(key) {
+      return this.data[key];
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashMap.prototype.remove = function(key) {
+      this.data[key] = -1;
+};
+
+/** 
+ * Your MyHashMap object will be instantiated and called as such:
+ * var obj = new MyHashMap()
+ * obj.put(key,value)
+ * var param_2 = obj.get(key)
+ * obj.remove(key)
+ */
