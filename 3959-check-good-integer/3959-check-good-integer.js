7e221fbfb79fd937076359f6a787ee6e0770d569
Mayank Mishra <mayank0801mishra@gmail.com>
2026-08-02 02:05:30 +0530

    Time: 1 ms (50.08%), Space: 55 MB (86.63%) - LeetHub

---
+++ 3959-check-good-integer/3959-check-good-integer.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var checkGoodInteger = function(n) {
+    let digitSum = 0;
+    let squareSum = 0;
+
+    while(n > 0){
+        let digit = n % 10;
+
+        digitSum += digit;
+        squareSum += digit*digit;
+
+        n = Math.floor(n/10);
+    }
+
+    return squareSum - digitSum >= 50;
+
+};
