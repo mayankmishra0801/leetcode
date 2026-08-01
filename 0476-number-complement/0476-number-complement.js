94f6463bbef61c7b11afe5ec97e79ec5bea95e82
Mayank Mishra <mayank0801mishra@gmail.com>
2026-08-01 14:43:45 +0530

    Time: 0 ms (100%), Space: 54.7 MB (7.27%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var findComplement = function(num) {
+    let mask = 1;
+
+    while(mask < num){
+        mask = (mask << 1) | 1;
+    }
+
+    return num ^ mask;
+};
