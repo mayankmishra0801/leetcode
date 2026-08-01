3f19a7199adf35f6d543c247a7be1e5fe74bc851
Mayank Mishra <mayank0801mishra@gmail.com>
2026-08-02 01:53:42 +0530

    Time: 1 ms (73.13%), Space: 56.5 MB (61.19%) - LeetHub

---
+++ 2778-sum-of-squares-of-special-elements/2778-sum-of-squares-of-special-elements.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var sumOfSquares = function(nums) {
+    const n = nums.length;
+
+    let sum = 0;
+
+
+    for(let i = 1;i<= n;i++){
+        if(n % i === 0){
+            sum += nums[i-1] * nums[i-1];
+        }
+    }
+
+    return sum;
+};
