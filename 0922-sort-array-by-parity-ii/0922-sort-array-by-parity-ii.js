0ab86cdad38873edf1d623f6269ccb1622af6276
Mayank Mishra <mayank0801mishra@gmail.com>
2026-08-02 21:02:25 +0530

    Time: 4 ms (68.05%), Space: 60.3 MB (76.69%) - LeetHub

---
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParityII = function(nums) {
+    let even = 0;
+
+    let odd = 1;
+
+    while(even < nums.length && odd < nums.length){
+        if(nums[even] % 2 === 0){
+            even += 2;
+
+        }else if(nums[odd] % 2 === 1){
+            odd += 2;
+        }else{
+            [nums[even],nums[odd]] = [nums[odd],nums[even]];
+
+            even += 2;
+            odd += 2;
+        }
+    }
+
+    return nums;
+};
