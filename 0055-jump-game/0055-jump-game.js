0c520f6dd47a3d62f292b242d1f46e74d1f01d5b
Mayank Mishra <mayank0801mishra@gmail.com>
2026-08-02 13:16:58 +0530

    Time: 3 ms (35.62%), Space: 59.5 MB (19.34%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    let reach = 0;
+
+    for(let i = 0; i< nums.length;i++){
+        if(i>reach) return false;
+
+        reach = Math.max(reach, i + nums[i]);
+    }
+
+
+    return true;
+};
